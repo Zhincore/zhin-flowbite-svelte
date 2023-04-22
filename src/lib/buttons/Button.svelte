c16de1d65dd2e922466c56325649167f277cd32b
Adam Žingor <adam@zhincore.eu>
2023-04-22 20:45:23 +0200

    Added transition to Buttons and united gradients

--- src/lib/buttons/Button.svelte
+++ src/lib/buttons/Button.svelte
@@ -5,39 +5,40 @@
 
   const group = getContext('group');
 
-  export let pill: boolean = false;
-  export let outline: boolean = false;
-  export let gradient: boolean = false;
+  export let pill = false;
+  export let outline = false;
+  export let gradient = false;
   export let size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' = group ? 'sm' : 'md';
   export let href: string | undefined = undefined;
   export let btnClass: string | undefined = undefined;
   export let type: ButtonType = 'button';
+  export let color: Color | Gradient = group ? (outline ? 'dark' : 'alternative') : 'blue';
+  export let shadow: Color | null = null;
 
-  export let color:
-    | 'alternative'
+  type Color =
     | 'blue'
     | 'cyan'
-    | 'dark'
-    | 'light'
     | 'lime'
     | 'green'
     | 'pink'
-    | 'primary'
     | 'red'
     | 'teal'
     | 'yellow'
     | 'purple'
+    | 'light'
+    | 'dark'
+    | 'primary'
+    | 'alternative';
+  type Gradient =
     | 'purpleToBlue'
     | 'cyanToBlue'
     | 'greenToBlue'
     | 'purpleToPink'
     | 'pinkToOrange'
     | 'tealToLime'
-    | 'redToYellow' = group ? (outline ? 'dark' : 'alternative') : 'blue';
+    | 'redToYellow';
 
-  export let shadow: 'blue' | 'green' | 'cyan' | 'teal' | 'lime' | 'red' | 'pink' | 'purple' | null = null;
-  
-  const colorClasses = {
+  const colorClasses: Partial<Record<Color, string>> = {
     blue: 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800',
     dark: 'text-white bg-gray-800 hover:bg-gray-900 focus:ring-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700',
     alternative:
@@ -54,34 +55,34 @@
       'text-white bg-purple-700 hover:bg-purple-800 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'
   };
 
-  const gradientClasses = {
-    blue: 'text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-blue-300 dark:focus:ring-blue-800 ',
+  const gradientClasses: Partial<Record<Color | Gradient, string>> = {
+    blue: 'text-white bg-dbl-gradient-to-br bg-200% from-blue-500 via-blue-600 to-blue-700 hover:bg-right-bottom bg-200% focus:ring-blue-300 dark:focus:ring-blue-800 ',
     green:
-      'text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-green-300 dark:focus:ring-green-800',
-    cyan: 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-cyan-300 dark:focus:ring-cyan-800',
-    teal: 'text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-teal-300 dark:focus:ring-teal-800',
-    lime: 'text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-lime-300 dark:focus:ring-lime-800',
-    red: 'text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-red-300 dark:focus:ring-red-800',
-    pink: 'text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-pink-300 dark:focus:ring-pink-800',
+      'text-white bg-dbl-gradient-to-br bg-200% from-green-400 via-green-500 to-green-600 hover:bg-right-bottom bg-200% focus:ring-green-300 dark:focus:ring-green-800',
+    cyan: 'text-white bg-dbl-gradient-to-br bg-200% from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-right-bottom bg-200% focus:ring-cyan-300 dark:focus:ring-cyan-800',
+    teal: 'text-white bg-dbl-gradient-to-br bg-200% from-teal-400 via-teal-500 to-teal-600 hover:bg-right-bottom bg-200% focus:ring-teal-300 dark:focus:ring-teal-800',
+    lime: 'text-gray-900 bg-dbl-gradient-to-br bg-200% from-lime-200 via-lime-400 to-lime-500 hover:bg-right-bottom bg-200% focus:ring-lime-300 dark:focus:ring-lime-800',
+    red: 'text-white bg-dbl-gradient-to-br bg-200% from-red-400 via-red-500 to-red-600 hover:bg-right-bottom bg-200% focus:ring-red-300 dark:focus:ring-red-800',
+    pink: 'text-white bg-dbl-gradient-to-br bg-200% from-pink-400 via-pink-500 to-pink-600 hover:bg-right-bottom bg-200% focus:ring-pink-300 dark:focus:ring-pink-800',
     purple:
-      'text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-purple-300 dark:focus:ring-purple-800',
+      'text-white bg-dbl-gradient-to-br bg-200% from-purple-500 via-purple-600 to-purple-700 hover:bg-right-bottom bg-200% focus:ring-purple-300 dark:focus:ring-purple-800',
     purpleToBlue:
-      'text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-blue-300 dark:focus:ring-blue-800',
+      'text-white bg-dbl-gradient-to-br bg-200% from-purple-600 to-blue-500 hover:bg-right-bottom focus:ring-blue-300 dark:focus:ring-blue-800',
     cyanToBlue:
-      'text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-cyan-300 dark:focus:ring-cyan-800',
+      'text-white bg-dbl-gradient-to-br bg-200% from-cyan-500 to-blue-500 hover:bg-right-bottom focus:ring-cyan-300 dark:focus:ring-cyan-800',
     greenToBlue:
-      'text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-green-200 dark:focus:ring-green-800',
+      'text-white bg-dbl-gradient-to-br bg-200% from-green-400 to-blue-600 hover:bg-right-bottom focus:ring-green-200 dark:focus:ring-green-800',
     purpleToPink:
-      'text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-purple-200 dark:focus:ring-purple-800',
+      'text-white bg-dbl-gradient-to-br bg-200% from-purple-500 to-pink-500 hover:bg-right-bottom focus:ring-purple-200 dark:focus:ring-purple-800',
     pinkToOrange:
-      'text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-pink-200 dark:focus:ring-pink-800',
+      'text-white bg-dbl-gradient-to-br bg-200% from-pink-500 to-orange-400 hover:bg-right-bottom focus:ring-pink-200 dark:focus:ring-pink-800',
     tealToLime:
-      'text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l focus:ring-lime-200 dark:focus:ring-teal-700',
+      'text-gray-900 bg-dbl-gradient-to-br bg-200% from-teal-200 to-lime-200 hover:bg-right-bottom focus:ring-lime-200 dark:focus:ring-teal-700',
     redToYellow:
-      'text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-red-100 dark:focus:ring-red-400'
+      'text-gray-900 bg-dbl-gradient-to-br bg-200% from-red-200 via-red-300 to-yellow-200 hover:bg-right-bottom focus:ring-red-100 dark:focus:ring-red-400'
   };
 
-  const coloredShadowClasses = {
+  const coloredShadowClasses: Partial<Record<Color, string>> = {
     blue: 'shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80',
     green: 'shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80',
     cyan: 'shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80',
@@ -92,7 +93,7 @@
     purple: 'shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80'
   };
 
-  const outlineClasses = {
+  const outlineClasses: Partial<Record<Color, string>> = {
     blue: 'text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800',
     light:
       'text-gray-500 hover:text-gray-900 bg-white border border-gray-200 dark:border-gray-600 dark:hover:text-white dark:text-gray-400 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-400',
@@ -114,7 +115,7 @@
     xl: 'px-6 py-3.5 text-base'
   };
 
-  function rounded(gradientOutline: boolean = false) {
+  function rounded(gradientOutline = false) {
     if (group) {
       return pill
         ? 'first:rounded-l-full last:rounded-r-full'
@@ -136,7 +137,11 @@
         group && 'focus:z-10',
         group || 'focus:outline-none',
         outline && gradient ? 'p-0.5' : 'inline-flex items-center justify-center ' + sizeClasses[size],
-        gradient ? gradientClasses[color] : outline ? outlineClasses[color] : colorClasses[color],
+        gradient
+          ? gradientClasses[color]
+          : outline
+          ? outlineClasses[color as Color]
+          : colorClasses[color as Color],
         color === 'alternative' &&
           (group
             ? 'dark:bg-gray-700 dark:text-white dark:border-gray-700 dark:hover:border-gray-600 dark:hover:bg-gray-600'
@@ -147,6 +152,7 @@
         hasBorder() && group && 'border-l-0 first:border-l',
         rounded(false),
         shadow && coloredShadowClasses[shadow],
+        'transition-all',
         $$props.disabled && 'cursor-not-allowed opacity-50',
         $$props.class
       );
@@ -157,7 +163,7 @@
     sizeClasses[size],
     rounded(true),
     'bg-white text-gray-900 dark:bg-gray-900 dark:text-white', // this is limitation - no transparency
-    'transition-all duration-75 ease-in group-hover:bg-opacity-0 group-hover:text-inherit'
+    'group-hover:bg-opacity-0 group-hover:text-inherit'
   );
 </script>
 
